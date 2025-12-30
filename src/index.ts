80630205df95036c9f716083bcc7380853397548
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-12-30 15:04:03 +0530

    p

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+let x = 1;
+
+app.get("/", (req, res) => {
+                res.json({
+            message: "Hi there",
+  });
+});
+
+app.get("/todo", (req, res) => {
+                res.json({
+            message: "Hi there",
+  });
+});
