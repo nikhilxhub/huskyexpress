d981e02bce2361499e05a5d8534d2e65ec5cf758
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-12-30 15:04:55 +0530

    p2

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ app.use(express.json());
 let x = 1;
 
 app.get("/", (req, res) => {
-                res.json({
-            message: "Hi there",
+  res.json({
+    message: "Hi there",
   });
 });
 
 app.get("/todo", (req, res) => {
-                res.json({
-            message: "Hi there",
-  });
+       res.json({
+              message: "Hi there",
+        });
 });
